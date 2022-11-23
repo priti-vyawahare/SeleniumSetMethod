f8ec1f8577651f4bfd58f33e87c91fc1b65f23ee
priti vyawahare <pritiv.acem@gmail.com>
2022-11-23 11:17:13 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Formtesting
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        [SetUp]
+        public void Intializedtest()
+        {
+            driver.Navigate().GoToUrl("https://www.workingtalent.nl/vacatures/it-traineeship-software-testing");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            // Entertext(element,value,type)
+
+            /* IWebElement element = driver.FindElement(By.Name("naam"));
+             element.SendKeys("priti");
+             IWebElement emailelement = driver.FindElement(By.Name("email"));
+             emailelement.SendKeys("pritiv.acem@gmail.com");
+              IWebElement phonenumber = driver.FindElement(By.Name("telefoonnummer"));
+             phonenumber.SendKeys("0675467482");
+            */
+            SeleniumSetMethod.EnterText(driver, "Name", "priti", "naam");
+            SeleniumSetMethod.EnterText(driver, "Name", "pritiv.acem@gmail.com", "email");
+           
+        }
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumSetMethod.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace Formtesting
+{
+    class SeleniumSetMethod
+    {
+        // Enter text
+        public static void EnterText(IWebDriver driver,string element, string value, string elementType)
+        {
+            if(elementType == "Name")
+            driver.FindElement(By.Name(element)).SendKeys(value);
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+        }
+        // Click into a button,checkbox,option etc
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).Click();
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).Click();
+
+        }
+        // Selecting Dropdown contrl
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string elementType)
+        {
+            if (elementType == "Name")
+               new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            if (elementType == "Id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+        }
+    }
+}
